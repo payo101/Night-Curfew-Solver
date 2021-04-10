79f70de553f0a9d8640f48200cb136d85fc9c42b
vxrun <65116944+vxrun@users.noreply.github.com>
2021-04-11 00:26:08 +0530

    commit

--- backend/config/user.js
+++ backend/config/user.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g9osi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
 const Schema = mongoose.Schema;
 
 const User = new Schema({
